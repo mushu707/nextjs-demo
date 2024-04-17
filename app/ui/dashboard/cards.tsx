bdaa44815fc163d3724e223171bd69a22ecb2519
mushu <1370826644@qq.com>
2024-04-17 16:20:40 +0800

    first push

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -20,9 +20,9 @@ export default async function CardWrapper() {
 
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card title="Total Page" value={numberOfInvoices} type="invoices" />
       <Card
-        title="Total Customers"
+        title="Total Page"
         value={numberOfCustomers}
         type="customers"
       /> */}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lustria } from "next/font/google";
+
+export const inter = Inter({subsets: ['latin']});
+
+export const lusitana = Lustria({weight: ['400'], subsets: ['latin']});
